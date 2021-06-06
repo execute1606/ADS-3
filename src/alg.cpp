b18c9c5ee9a2a5a377e752bef6181b2f8122a3e7
execute1606 <72761350+execute1606@users.noreply.github.com>
2021-06-06 23:40:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,103 +1,127 @@
 // Copyright 2021 NNTU-CS
 #include <string>
-#include <stack>
-#include <map>
+#include "tstack.h"
 
+int math(int first, int second, char symbol) {
+    switch (symbol) {
+    case '*':
+    {
+        second *= first;
+        return second;
+    }
+    case '/':
+    {
+        second /= first;
+        return second;
+    }
+    case '+':
+    {
+        second += first;
+        return second;
+    }
+    case '-':
+    {
+        second -= first;
+        return second;
+    }
+    default:
+        break;
+    }
+    return second;
+}
+int priority(char chr) {
+    int prior = 0;
+    if (chr == '(')
+        prior = 0;
+    if (chr == ')')
+        prior = 1;
+    if (chr == '+' || chr == '-')
+        prior = 2;
+    if (chr == '*' || chr == '/')
+        prior = 3;
+    return prior;
+}
 std::string infx2pstfx(std::string inf) {
-    std::map<char, int> prior = { {'(', 0}, {')', 1},
-        {'+', 2}, {'-', 2}, {'*', 3}, {'/', 3} };
-    std::string res, temp;
-    std::stack<char> stack;
-    for (auto& chr : inf) {
+    // добавьте сюда нужный код
+    TStack<char> stack1;
+    std::string res;
+    int prior = -1;
+    int priorTop = 0;
+    char chr = ' ';
+    char chrPrv = ' ';
+    bool firstNumber = 0;
+    for (int i = 0; i < inf.length(); i++) {
+        chr = inf[i];
+        chrPrv = stack1.get();
         if (chr >= '0' && chr <= '9') {
-            temp += chr;
-        }
-        else if (chr == '(' || chr == '+' ||
-            chr == '-' || chr == '*' || chr == '/') {
-            if (temp.length()) {
-                res += temp;
-                res += ' ';
-                temp = "";
-            }
-            if (!stack.empty()) {
-                if (chr == '(') {
-                    stack.push(chr);
-                }
-                else if (prior[chr] > prior[stack.top()]) {
-                    stack.push(chr);
-                }
-                else {
-                    res += stack.top();
-                    stack.pop();
-                    stack.push(chr);
-                    res += ' ';
-                }
+            if (!firstNumber) {
+                firstNumber = true;
             }
             else {
-                stack.push(chr);
+                res.push_back(' ');
             }
+            res.push_back(chr);
         }
-        else if (chr == ')') {
-            if (temp.length()) {
-                res += temp;
-                res += ' ';
-                temp = "";
+        else {
+            prior = priority(chr);
+            priorTop = priority(chrPrv);
+            if ((stack1.isEmpty() || prior > priorTop || !prior)
+                && prior != 1) {
+                stack1.push(chr);
             }
-            if (!stack.empty()) {
-                while (stack.top() != '(') {
-                    res += stack.top();
-                    stack.pop();
-                    res += ' ';
+            else {
+                if (prior == 1) {
+                    while (stack1.get() != '(') {
+                        res.push_back(' ');
+                        res.push_back(stack1.get());
+                        stack1.pop();
+                    }
+                    stack1.pop();
                 }
-                stack.pop();
+                else {
+                    if (prior <= priorTop && prior > 1) {
+                        while (!stack1.isEmpty() && stack1.get() != '(') {
+                            res.push_back(' ');
+                            res.push_back(stack1.get());
+                            stack1.pop();
+                        }
+                        stack1.push(chr);
+                    }
+                }
             }
         }
     }
-    if (temp.length()) {
-        res += temp;
-        res += ' ';
+    while (!stack1.isEmpty()) {
+        res.push_back(' ');
+        res.push_back(stack1.get());
+        stack1.pop();
     }
-    for (unsigned int i = stack.size(); i > 0; i--) {
-        res += stack.top();
-        res += ' ';
-        stack.pop();
-    }
-    res.erase(res.end() - 1, res.end());
     return res;
 }
 
 int eval(std::string pst) {
-    unsigned int x = 0, y = 0;
-    std::stack<unsigned int> stack;
-    unsigned int temp = 0;
-    for (unsigned int i = 0; i < pst.length() + 1; i++) {
-        if (pst[i] >= '0' && pst[i] <= '9') {
-            temp *= 10;
-            temp += pst[i] - '0';
-        }
-        else if (pst[i] == ' ') {
-            stack.push(temp);
-            temp = 0;
-        }
-        else {
-            x = stack.top();
-            stack.pop();
-            y = stack.top();
-            stack.pop();
-            if (pst[i] == '+') {
-                stack.push(y + x);
-            }
-            else if (pst[i] == '-') {
-                stack.push(y - x);
-            }
-            else if (pst[i] == '*') {
-                stack.push(y * x);
+    int result = 0;
+    TStack<char> stack1;
+    TStack<int> stack2;
+    int prior = 0, priorPrv = 0;
+    int first = 0, second = 0;
+    char chr = ' ';
+    char chrPrv = ' ';
+    for (int i = 0; i < pst.length(); i++) {
+        chr = pst[i];
+        if (chr != ' ') {
+            if (chr >= '0' && chr <= '9') {
+                stack2.push(chr - 48);
             }
             else {
-                stack.push(y / x);
+                first = stack2.get();
+                stack2.pop();
+                second = stack2.get();
+                stack2.pop();
+                second = math(first, second, chr);
+                stack2.push(second);
             }
-            i++;
         }
     }
-    return stack.top();
+    return stack2.get();
 }
